c7ba5509b16388380cae5ff38673b4e9a116b01e
Mahdi Mashayekhi <MahdiMashayekhi@gmail.com>
2023-03-29 16:43:40 +0330

    Add templates to authors and books app also set urls and create views for project.

--- authors/views.py
+++ authors/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Author
 
 # Create your views here.
+
+def authors_list(request):
+    authors = Author.objects.all()
+
+    context = {
+        'authors': authors,
+    }
+
+    return render(request, 'books_list.html', context)
--- books/views.py
+++ books/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
+
+def books_list(request):
+    books = Book.objects.all()
+
+    context = {
+        'books': books,
+    }
+
+    return render(request, 'books_list.html', context)
