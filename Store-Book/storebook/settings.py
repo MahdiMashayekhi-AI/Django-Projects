92f5b2d1ff9764fefab5193c3dde8cc1b3e27cde
Mahdi Mashayekhi <MahdiMashayekhi@gmail.com>
2023-03-31 12:37:51 +0330

    Change database from sqlite to mysql.

--- Store-Book/storebook/settings.py
+++ Store-Book/storebook/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'storebook.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bookstore',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOst': 'localhost',
+        'PORT': '3306',
     }
 }
 
