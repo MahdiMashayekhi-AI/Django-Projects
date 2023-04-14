e2ef9572e4eaeb18b810662b0ee23547540cd4de
Mahdi Mashayekhi <MahdiMashayekhi@gmail.com>
2023-04-14 11:21:35 +0330

    Delete some files

--- BookStore/authors/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('authors/', views.authors_list , name='authors_list')
-]
--- BookStore/books/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('books/', views.books_list , name='books_list')
-]
